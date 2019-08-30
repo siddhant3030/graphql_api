5fdd3fedd22ea7d60c5f9bd57c90cc39f9d178aa
siddhant3030 <ssiddhant3030@gmail.com>
2019-08-30 19:25:02 +0530

    Signup added

--- mix.exs
+++ mix.exs
@@ -40,7 +40,9 @@ defmodule MediumGraphql.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:comeonin, "~> 4.0"},
+      {:argon2_elixir, "~> 1.2"}
     ]
   end
 
